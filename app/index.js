d0141139b451ab1371c49621aab724c2fa728ff6
Jered McGlohon <johon8@gmail.com>
2018-01-31 16:12:25 -0500

    updated to new generator format

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 //<%= ngapp %> to add text in file
 'use strict';
-var generator = require('yeoman-generator'),
+var Generator = require('yeoman-generator'),
     chalk = require('chalk'),
     yosay = require('yosay'),
     includes = function (ary, lib) {
@@ -9,18 +9,18 @@ var generator = require('yeoman-generator'),
         return val > -1;
     };
 
-module.exports = generator.extend({
-    constructor: function(){
-        generator.apply(this, arguments);
+module.exports = class extends Generator{
+    constructor(args, opts) {
+        super(args, opts);
         
         this.includes = includes;
         // this.argument('appname', { type: String, required: true });
         // this.appname = _.kebabCase(this.appname);
         
-    },  
-    initializing: function(){
-    },
-    prompting: function(){
+    };
+    initializing() {
+    };
+    prompting() {
         var self = this;
 
         this.log(yosay(chalk.yellow('Create an ES6 project')));
@@ -95,107 +95,105 @@ module.exports = generator.extend({
             //done(); 
         });
             
-    },
-    configuring: function(){
-    },
-    writing: {
-        packageJSON: function(){
-            var packageFile = {
-                name: this.projectName,
-                version: "1.0.0",
-                description: this.desc,
-                main: "app.ts",
-                scripts: {
-                    "devBuild": "webpack --config ./webpackConfigs/webpack.config.dev.js",
-                    "prodBuild": "webpack --config ./webpackConfigs/webpack.config.prod.js"
-                },
-                author: this.author,
-                license: "ISC",
-                dependencies: {},
-                devDependencies: {}
-            };
-
-            //dependencies
-            if(this.includeAxios) {packageFile.dependencies["axios"] = "^0.17.0";}
-            if(this.includeJquery) {packageFile.dependencies["jquery"] = "^3.0.0";}
-            if(this.includeJquery) {packageFile.devDependencies["@types/jquery"] = "^3.0.0";}
-            if(this.includeLodash) {packageFile.dependencies["lodash"] = "^4.0.0";}
-            if(this.includeLodash) {packageFile.devDependencies["@types/lodash"] = "^4.0.0";}
-            if(this.includeMoment) {packageFile.dependencies["moment"] = "^2.0.0";}
-            if(this.includesputil) {packageFile.dependencies["pd-sputil"] = "^2.0.0";}
-            if(this.includespserverajax) {packageFile.dependencies["pd-spserverajax"] = "^2.0.0";}
-            if(this.includespserverjsom) {packageFile.dependencies["pd-spserverjsom"] = "^1.0.0";}
-            if(this.includeapputil) {packageFile.dependencies["pd-apputil"] = "^1.0.0";}
-            packageFile.dependencies["core-js"] = "^2.0.0";
-            packageFile.devDependencies["@types/core-js"]= "^0.9.43"
-            
-            //devDependencies
-            packageFile.devDependencies["webpack"] = "^3.0.0";
-            packageFile.devDependencies["clean-webpack-plugin"] = "^0.1.17";
-            packageFile.devDependencies["html-webpack-plugin"] = "^2.30.1";
-            packageFile.devDependencies["css-loader"] = "0.28.7";
-            packageFile.devDependencies["sass-loader"] = "^6.0.6";
-            packageFile.devDependencies["file-loader"] = "^1.1.5";
-            packageFile.devDependencies["style-loader"] = "0.19.0";
-            packageFile.devDependencies["ts-loader"] = "^3.0.0";
-            packageFile.devDependencies["extract-text-webpack-plugin"] = "^3.0.0";
-            packageFile.devDependencies["typescript"] = "^2.0.0";
-            //packageFile.devDependencies["tslint"] = "latest";
-            packageFile.devDependencies["npm-run-all"] = "^4.1.1";
-
-            this.fs.writeJSON(
-                this.destinationPath('package.json'),
-                packageFile
-            );
-        },
-        appStaticFiles: function(){
-            // this.copy('_favicon.ico', 'src/favicon.ico');
-            this.fs.copy(
-                this.templatePath('gitignore'),
-                this.destinationPath('.gitignore')
-            );
-            this.fs.copy(
-                this.templatePath('tslint.json'),
-                this.destinationPath('tslint.json')
-            );
-            this.fs.copy(
-                this.templatePath('webpackConfigs/**'),
-                this.destinationPath('webpackConfigs/')
-            );
-            this.fs.copy(
-                this.templatePath('tsconfig.json'),
-                this.destinationPath('tsconfig.json')
-            );
-        },
-        styleSheets: function() {
-            this.fs.copy(
-                this.templatePath('app/_main.scss'),
-                this.destinationPath('src/styleSheets/main.scss')
-            );
-        },
-        scripts: function(){
-            this.fs.copyTpl(
-                this.templatePath('app/_app.ts'),
-                this.destinationPath('src/scripts/app.ts'),
-                {
-                    projectName: this.projectName
-                }
-            );
-        },
-        html: function(){
-            this.fs.copy(
-                this.templatePath('_index.html'),
-                this.destinationPath('src/index.html')
-            );
-        }
-    },
-    conflicts: function(){
-    },
-    install: function(){
+    };
+    configuring() {
+    };
+    createPackageJSON() {
+        var packageFile = {
+            name: this.projectName,
+            version: "1.0.0",
+            description: this.desc,
+            main: "app.ts",
+            scripts: {
+                "devBuild": "webpack --config ./webpackConfigs/webpack.config.dev.js",
+                "prodBuild": "webpack --config ./webpackConfigs/webpack.config.prod.js"
+            },
+            author: this.author,
+            license: "ISC",
+            dependencies: {},
+            devDependencies: {}
+        };
+    
+        //dependencies
+        if(this.includeAxios) {packageFile.dependencies["axios"] = "^0.17.0";}
+        if(this.includeJquery) {packageFile.dependencies["jquery"] = "^3.0.0";}
+        if(this.includeJquery) {packageFile.devDependencies["@types/jquery"] = "^3.0.0";}
+        if(this.includeLodash) {packageFile.dependencies["lodash"] = "^4.0.0";}
+        if(this.includeLodash) {packageFile.devDependencies["@types/lodash"] = "^4.0.0";}
+        if(this.includeMoment) {packageFile.dependencies["moment"] = "^2.0.0";}
+        if(this.includesputil) {packageFile.dependencies["pd-sputil"] = "^2.0.0";}
+        if(this.includespserverajax) {packageFile.dependencies["pd-spserverajax"] = "^2.0.0";}
+        if(this.includespserverjsom) {packageFile.dependencies["pd-spserverjsom"] = "^1.0.0";}
+        if(this.includeapputil) {packageFile.dependencies["pd-apputil"] = "^1.0.0";}
+        packageFile.dependencies["core-js"] = "^2.0.0";
+        packageFile.devDependencies["@types/core-js"]= "^0.9.43"
+        
+        //devDependencies
+        packageFile.devDependencies["webpack"] = "^3.0.0";
+        packageFile.devDependencies["clean-webpack-plugin"] = "^0.1.17";
+        packageFile.devDependencies["html-webpack-plugin"] = "^2.30.1";
+        packageFile.devDependencies["css-loader"] = "0.28.7";
+        packageFile.devDependencies["sass-loader"] = "^6.0.6";
+        packageFile.devDependencies["file-loader"] = "^1.1.5";
+        packageFile.devDependencies["style-loader"] = "0.19.0";
+        packageFile.devDependencies["ts-loader"] = "^3.0.0";
+        packageFile.devDependencies["extract-text-webpack-plugin"] = "^3.0.0";
+        packageFile.devDependencies["typescript"] = "^2.0.0";
+        //packageFile.devDependencies["tslint"] = "latest";
+        packageFile.devDependencies["npm-run-all"] = "^4.1.1";
+    
+        this.fs.writeJSON(
+            this.destinationPath('package.json'),
+            packageFile
+        );
+    };
+    appStaticFiles() {
+        // this.copy('_favicon.ico', 'src/favicon.ico');
+        this.fs.copy(
+            this.templatePath('gitignore'),
+            this.destinationPath('.gitignore')
+        );
+        this.fs.copy(
+            this.templatePath('tslint.json'),
+            this.destinationPath('tslint.json')
+        );
+        this.fs.copy(
+            this.templatePath('webpackConfigs/**'),
+            this.destinationPath('webpackConfigs/')
+        );
+        this.fs.copy(
+            this.templatePath('tsconfig.json'),
+            this.destinationPath('tsconfig.json')
+        );
+    };
+    styleSheets() {
+        this.fs.copy(
+            this.templatePath('app/_main.scss'),
+            this.destinationPath('src/styleSheets/main.scss')
+        );
+    };
+    scripts() {
+        this.fs.copyTpl(
+            this.templatePath('app/_app.ts'),
+            this.destinationPath('src/scripts/app.ts'),
+            {
+                projectName: this.projectName
+            }
+        );
+    };
+    html() {
+        this.fs.copy(
+            this.templatePath('_index.html'),
+            this.destinationPath('src/index.html')
+        );
+    };
+    conflicts() {
+    };
+    install() {
         //this.bowerInstall();
         this.npmInstall();
-    },
-    end: function(){
+    };
+    end() {
         this.log(chalk.yellow.bold('Installation successful!'));
-    }
-});
+    };
+};
